9b4fc5fdbe03ff6dc1c30dcad184edc5ea4bbe85
José Carneiro <josemcarneiro@gmail.com>
2020-05-27 10:50:33 +0100

    Added examples with setting array in state and manipulating it

--- react-iteration/src/App.jsx
+++ react-iteration/src/App.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import students from './students.json';
+
 const StudentInfo = props => {
-  console.log(props);
   return (
     <div>
       <h3>{props.name}</h3>
@@ -15,21 +16,35 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      message: '',
       people: [
         { name: 'José', age: 27 },
         { name: 'Santi', age: 30 },
         { name: 'Aline', age: 29 }
       ],
-      students: [
-        { name: 'Vasco', age: 30 },
-        { name: 'Fábio', age: 26 },
-        { name: 'Ricardo Loureiro', age: 22 }
-      ]
+      students: [...students]
     };
   }
 
+  randomizeStudents = () => {
+    const students = [...this.state.students];
+    students.sort(() => 0.5 - Math.random());
+    this.setState({
+      students: students
+    });
+  };
+
+  removeLastStudent = () => {
+    const students = [...this.state.students];
+    students.pop();
+    this.setState({
+      students: students
+    });
+  };
+
   render() {
     const people = this.state.people;
+
     return (
       <div className="App">
         <ul>
@@ -46,7 +61,10 @@ class App extends Component {
           return <StudentInfo key={student.name} name={student.name} age={student.age} />;
         })}
 
-        {this.state.students.map(student => <StudentInfo key={student.name} {...student} />)}
+        {/* {this.state.students.map(student => <StudentInfo key={student.name} {...student} />)} */}
+
+        <button onClick={this.randomizeStudents}>Randomized</button>
+        <button onClick={this.removeLastStudent}>Remove last student</button>
       </div>
     );
   }
